6fdbad7627d8fbc6b6fdd5b9fe02a865d7c2fd26
AthulJohn <athulpulickaljohn@gmail.com>
2022-07-11 11:07:36 +0530

    get state changes tadaaaa

--- lib/Screens/TabScreens/order_page.dart
+++ lib/Screens/TabScreens/order_page.dart
@@ -11,130 +11,130 @@ class OrderPage extends StatelessWidget {
   OrderController oc = Get.put(OrderController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: // WillPopScope(
-          // onWillPop: () async {
-          //   Get.back<InvoiceData>(result: inv);
-          //   return true;
-          // },
-          // child:
-          Container(
-        decoration: BoxDecoration(
-          color: Color.fromARGB(69, 224, 224, 254),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 80,
+    return Obx(() => Scaffold(
+          body: // WillPopScope(
+              // onWillPop: () async {
+              //   Get.back<InvoiceData>(result: inv);
+              //   return true;
+              // },
+              // child:
+              Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(69, 224, 224, 254),
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 SizedBox(
-                  width: 43,
+                  height: 80,
                 ),
-                Image(
-                  width: 800,
-                  image: AssetImage('assets/images/invoice-banner.png'),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            formqstn(
-              data: 'Order Number',
-              hint: 'Order Number',
-              onchanged: (text) {
-                inv.orderNo = text;
-              },
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text('Orders'),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    child: Text('Add Order'),
-                    onPressed: () async {
-                      oc.add();
-                      oc.update();
-                      print("len${oc.items.length}");
-                    },
-                  ),
-                )
-              ],
-            ),
-            Expanded(
-                child: ListView.builder(
-              itemCount: oc.items.length,
-              itemBuilder: (context, ind) {
-                return Row(
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: formqstn(
-                        data: 'Item',
-                        hint: 'Enter Item Name',
-                        onchanged: (text) {
-                          oc.editName(text, ind);
-                        },
-                      ),
+                    SizedBox(
+                      width: 43,
                     ),
-                    Expanded(
-                      child: formqstn(
-                        data: 'Quantity',
-                        hint: 'Enter Quantity',
-                        onchanged: (text) {
-                          oc.editQuantity(double.tryParse(text) ?? 0, ind);
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: formqstn(
-                        data: 'Rate',
-                        hint: 'Enter Rate',
-                        onchanged: (text) {
-                          oc.editRate(double.tryParse(text) ?? 0, ind);
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: Text('${oc.totalprice.value}'),
+                    Image(
+                      width: 800,
+                      image: AssetImage('assets/images/invoice-banner.png'),
                     ),
                   ],
-                );
-              },
-            )),
-            Row(
-              children: [
-                Expanded(
-                  child: Text('${inv.amount}'),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                formqstn(
+                  data: 'Order Number',
+                  hint: 'Order Number',
+                  onchanged: (text) {
+                    inv.orderNo = text;
+                  },
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('Orders'),
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        child: Text('Add Order'),
+                        onPressed: () async {
+                          oc.add();
+                          oc.update();
+                          print("len${oc.items.length}");
+                        },
+                      ),
+                    )
+                  ],
                 ),
                 Expanded(
-                  child: formqstn(
-                    data: 'Tax Percent',
-                    hint: 'Enter Tax Percent',
-                    onchanged: (text) {
-                      inv.taxPercent = double.tryParse(text) ?? 0;
+                    child: ListView.builder(
+                  itemCount: oc.items.length,
+                  itemBuilder: (context, ind) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: formqstn(
+                            data: 'Item',
+                            hint: 'Enter Item Name',
+                            onchanged: (text) {
+                              oc.editName(text, ind);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: formqstn(
+                            data: 'Quantity',
+                            hint: 'Enter Quantity',
+                            onchanged: (text) {
+                              oc.editQuantity(double.tryParse(text) ?? 0, ind);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: formqstn(
+                            data: 'Rate',
+                            hint: 'Enter Rate',
+                            onchanged: (text) {
+                              oc.editRate(double.tryParse(text) ?? 0, ind);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: Text('${oc.totalprice.value}'),
+                        ),
+                      ],
+                    );
+                  },
+                )),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('${inv.amount}'),
+                    ),
+                    Expanded(
+                      child: formqstn(
+                        data: 'Tax Percent',
+                        hint: 'Enter Tax Percent',
+                        onchanged: (text) {
+                          inv.taxPercent = double.tryParse(text) ?? 0;
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: Text('${inv.amount}'),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Get.back<InvoiceData>(result: inv);
                     },
-                  ),
-                ),
-                Expanded(
-                  child: Text('${inv.amount}'),
-                ),
+                    child: Text('Add Order'))
               ],
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.back<InvoiceData>(result: inv);
-                },
-                child: Text('Add Order'))
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
